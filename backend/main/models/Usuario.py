9e32f9c618cdbd8282be046e2f3cfb337c864420
Danilo142 <d.cerna@alumno.um.edu.ar>
2022-04-12 19:43:41 -0300

    avance en clase

---
+++ backend/main/models/Usuario.py
@@ -0,0 +1,44 @@
+from .. import db
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    roll = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return '<Usuario: %r %r >' % (self.firstname, self.lastname, self.password, self.email, self.roll)
+    
+    def to_json(self):
+        usuario_json = {
+            'id': self.id,
+            'firstname': str(self.firstname),
+            'lastname': str(self.lastname),
+            'password': str(self.password),
+            'roll': str(self.roll),
+            'email': str(self.email),
+
+        }
+        return usuario_json
+
+    def to_json_short(self):
+        usuario_json = {
+            'id': self.id,
+            'lastname': str(self.lastname),
+            'password': str(self.password),
+            
+
+
+        }
+        return usuario_json
+    @staticmethod
+    
+    def from_json(usuario_json):
+        id = usuario_json.get('id')
+        firstname = usuario_json.get('firstname')
+        lastname = usuario_json.get('lastname')
+        return Usuario(id=id,
+                    firstname=firstname,
+                    lastname=lastname,
+                    )
--- backend/main/resources/poema.py
+++ backend/main/resources/poema.py
@@ -3,11 +3,11 @@ from flask import request
 
 #Prueba 
 POEMS = {
-    1 : {'Título' : 'La canción del pirata' ,'Autor' : 'José de Espronceda'},
-    2 : {'Título' : 'A una rosa' ,'Autor' : 'Gongora'},
-    3 : {'Título' : 'Ir y quedarse' ,'Autor' : 'Lope de Vega'},
-    4 : {'Título' : 'Hija del viento' ,'Autor' : 'Alejandra Pizarnik'},
-    5 : {'Título' : 'El remordimiento' ,'Autor' : 'Jorge Luis Borges'}
+    1 : {'Titulo' : 'La cancion del pirata' ,'Autor' : 'Jose de Espronceda'},
+    2 : {'Titulo' : 'A una rosa' ,'Autor' : 'Gongora'},
+    3 : {'Titulo' : 'Ir y quedarse' ,'Autor' : 'Lope de Vega'},
+    4 : {'Titulo' : 'Hija del viento' ,'Autor' : 'Alejandra Pizarnik'},
+    5 : {'Titulo' : 'El remordimiento' ,'Autor' : 'Jorge Luis Borges'}
 }
 
 class Poem(Resource):
